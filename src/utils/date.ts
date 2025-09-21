76526707bc75ba977834e56cec6fc9072439f1b4
Filipe Silva <filipe.silva@yacoobalabs.com>
2025-09-21 19:26:46 +0100

    chore: updated time constants

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,11 +1,19 @@
+import {
+  SECONDS_IN_A_YEAR,
+  SECONDS_IN_A_MONTH,
+  SECONDS_IN_A_DAY,
+  SECONDS_IN_AN_HOUR,
+  SECONDS_IN_A_MINUTE,
+} from './constants'
+
 export const timeAgo = (unixSeconds: number) => {
   const seconds = Math.floor(Date.now() / 1000) - unixSeconds
   const intervals = [
-    { label: 'year', sec: 31536000 },
-    { label: 'month', sec: 2592000 },
-    { label: 'day', sec: 86400 },
-    { label: 'hour', sec: 3600 },
-    { label: 'minute', sec: 60 },
+    { label: 'year', sec: SECONDS_IN_A_YEAR },
+    { label: 'month', sec: SECONDS_IN_A_MONTH },
+    { label: 'day', sec: SECONDS_IN_A_DAY },
+    { label: 'hour', sec: SECONDS_IN_AN_HOUR },
+    { label: 'minute', sec: SECONDS_IN_A_MINUTE },
     { label: 'second', sec: 1 },
   ]
 
